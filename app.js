937b4e3e1d0b97a592bf1dc62ec269093b5ae5dc
JunbyeongChae <wnsqud4545@gmail.com>
2024-12-22 16:51:19 +0900

    api키 한곳에 모음, app.js에 firebase.js호출 삭제

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -8,6 +9,10 @@ app.set("view engine", "ejs");
 
 // 정적 파일 제공
 app.use(express.static(path.join(__dirname, "public")));
+
+// API 키 관련 라우터 설정
+const apiKeysRouter = require("./config/apiKeys");
+app.use("/config", apiKeysRouter);
 
 // 라우트 설정
 app.get("/", (req, res) => res.render("pages/home")); // 홈 화면
